e49829fe14d6204a5a4c0e4c4faf992c8acc51e7
Brigita Macaitė <brigita.macaite@swedbank.lt>
2022-06-03 14:48:49 +0300

    feat: inject http

--- 01-first-component/app/app.module.ts
+++ 01-first-component/app/app.module.ts
@@ -3,23 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 // containers
 import { PassengerDashboardComponent } from './passenger-dashboard/containers/passenger-dasboard/passenger-dashboard.component';
 // components
 import { PassengerDetailComponent } from './passenger-dashboard/components/passenger-detail/passenger-detail.component';
 import { PassengerCountComponent } from './passenger-dashboard/components/passanger-count/passenger-count.component';
-import { PassengerDashboardService } from './passenger-dashboard/models/passenger-dashboard.service';
-
 // service
-
-
+import { PassengerDashboardService } from './passenger-dashboard/models/passenger-dashboard.service';
 
 
 @NgModule({
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   bootstrap: [
     AppComponent
